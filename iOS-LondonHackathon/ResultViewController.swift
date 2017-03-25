21fbe1957387f90634e20744ec8fc2bf41297c21
riku arakawa <a_ra_ra_rikky0611@yahoo.co.jp>
2017-03-25 22:30:27 +0000

    Add ResultViewController

---
+++ iOS-LondonHackathon/ResultViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  iOS-LondonHackathon
+//
+//  Created by 荒川陸 on 2017/03/25.
+//  Copyright © 2017年 Riku Arakawa. All rights reserved.
+//
+
+import UIKit
+import Koloda
+
+class ResultViewController: UIViewController {
+    
+    @IBOutlet weak var kolodaView: KolodaView!
+    var savedIdList:[Int] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        kolodaView.dataSource = self
+        kolodaView.delegate = self
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+extension ResultViewController: KolodaViewDelegate {
+    
+    func koloda(_ koloda: KolodaView, didSwipeCardAt index: Int, in direction: SwipeResultDirection) {
+        
+        if direction == .left {
+
+        } else if direction == .right {
+            
+            savedIdList.append(index+1)
+        }
+    }
+    
+    func kolodaDidRunOutOfCards(_ koloda: KolodaView) {
+        
+        print("run out of cards")
+    }
+}
+
+extension ResultViewController: KolodaViewDataSource {
+    
+    func kolodaNumberOfCards(_ koloda: KolodaView) -> Int {
+
+        return Comment.demoComments.count
+    }
+    
+    func koloda(_ koloda: KolodaView, viewForCardAt index: Int) -> UIView {
+        
+        let view = Bundle.main.loadNibNamed("KolodaCardView", owner: self, options: nil)?.first as! KolodaCardView
+        view.configure(comment: Comment.demoComments[index])
+        return view
+    }
+    
+    func koloda(_ koloda: KolodaView, viewForCardOverlayAt index: Int) -> OverlayView? {
+        
+        return Bundle.main.loadNibNamed("KolodaOverlayView", owner: self, options: nil)?[0] as? OverlayView
+    }
+}
+
