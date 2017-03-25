b8cefabd545a2cdb7386dae1184967787a3c0119
riku arakawa <a_ra_ra_rikky0611@yahoo.co.jp>
2017-03-25 23:27:30 +0000

    Create CommentTableViewController

---
+++ iOS-LondonHackathon/CommentViewController.swift
@@ -0,0 +1,50 @@
+//
+//  CommentViewController.swift
+//  iOS-LondonHackathon
+//
+//  Created by 荒川陸 on 2017/03/25.
+//  Copyright © 2017年 Riku Arakawa. All rights reserved.
+//
+
+import UIKit
+
+class CommentViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        tableView.reloadData()
+    }
+    
+}
+
+extension CommentViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int  {
+        
+        return Comment.savedComments.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath:IndexPath) -> UITableViewCell {
+        
+        let cell = Bundle.main.loadNibNamed("CommentTableViewCell", owner: self, options: nil)?.first as! CommentTableViewCell
+        let comment = Comment.savedComments[indexPath.row]
+        cell.configure(comment: comment)
+        return cell
+    }
+}
+
+extension CommentViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath:IndexPath) {
+        
+        print("\(indexPath.row)")
+    }
+}
