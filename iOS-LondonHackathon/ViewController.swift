5e972409f55c3c4aabaca3c5f000495b3214f1cf
riku arakawa <a_ra_ra_rikky0611@yahoo.co.jp>
2017-03-25 21:17:43 +0000

    Create project

---
+++ iOS-LondonHackathon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS-LondonHackathon
+//
+//  Created by 荒川陸 on 2017/03/25.
+//  Copyright © 2017年 Riku Arakawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
