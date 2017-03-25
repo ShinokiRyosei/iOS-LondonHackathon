9e9c74ff1d3f392c3485b16326541a714c514725
riku arakawa <a_ra_ra_rikky0611@yahoo.co.jp>
2017-03-25 23:26:02 +0000

    Change Comment Class

--- iOS-LondonHackathon/Comment.swift
+++ iOS-LondonHackathon/Comment.swift
@@ -8,21 +8,36 @@
 
 import Foundation
 
-struct Comment {
+class Comment {
     
     var text: String
+    var author: String
+    var point: Int
     var isHelpful: Bool?
     
-    init(text: String) {
+    init(text: String, author: String, point: Int) {
         self.text = text
+        self.author = author
+        self.point = point
+        self.isHelpful = false
     }
     
     static var demoComments = [
-        Comment(text: "aaa"),
-        Comment(text: "bbb"),
-        Comment(text: "ccc"),
-        Comment(text: "ddd"),
-        Comment(text: "eee")
+        Comment(text: "aaa", author: "testA", point: 3),
+        Comment(text: "bbb", author: "testB", point: 2),
+        Comment(text: "ccc", author: "testC", point: 5),
+        Comment(text: "ddd", author: "testD", point: 4),
+        Comment(text: "eee", author: "testE", point: 7)
     ]
+    
+    static var savedComments: [Comment] {
+        var ret: [Comment] = []
+        for comment in self.demoComments {
+            if comment.isHelpful! {
+                ret.append(comment)
+            }
+        }
+        return ret
+    }
     
 }
