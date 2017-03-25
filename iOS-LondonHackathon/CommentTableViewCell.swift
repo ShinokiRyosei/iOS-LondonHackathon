b4a61c41f4eab076a87da2edc25ccf4130337e6c
riku arakawa <a_ra_ra_rikky0611@yahoo.co.jp>
2017-03-25 23:26:19 +0000

    Add CommentTableViewCell

---
+++ iOS-LondonHackathon/CommentTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  ComentTableViewCell.swift
+//  iOS-LondonHackathon
+//
+//  Created by 荒川陸 on 2017/03/25.
+//  Copyright © 2017年 Riku Arakawa. All rights reserved.
+//
+
+import UIKit
+
+class CommentTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var pointLabel: UILabel!
+    @IBOutlet weak var label: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    func configure(comment: Comment) {
+        authorLabel.text = comment.author
+        label.text = comment.text
+        pointLabel.text = String(comment.point)
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
