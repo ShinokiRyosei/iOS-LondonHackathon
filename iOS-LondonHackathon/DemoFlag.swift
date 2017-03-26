735c261602fcd8b6b3b92eadc3c2f86801b44d67
riku arakawa <a_ra_ra_rikky0611@yahoo.co.jp>
2017-03-26 10:47:33 +0100

    Create DemoFlag Enum

---
+++ iOS-LondonHackathon/DemoFlag.swift
@@ -0,0 +1,15 @@
+//
+//  demoFlag.swift
+//  iOS-LondonHackathon
+//
+//  Created by 荒川陸 on 2017/03/26.
+//  Copyright © 2017年 Riku Arakawa. All rights reserved.
+//
+
+import Foundation
+
+enum DemoFlag {
+    case normalPost
+    case badPost
+    case newPost
+}
