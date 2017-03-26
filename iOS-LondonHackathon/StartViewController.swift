e7ef60daa8a4c840d322cc916c29e2e2020adb72
riku arakawa <a_ra_ra_rikky0611@yahoo.co.jp>
2017-03-26 10:48:10 +0100

    Implement each demo situation in StartViewController

--- iOS-LondonHackathon/StartViewController.swift
+++ iOS-LondonHackathon/StartViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 import SVProgressHUD
+import SCLAlertView
 
 class StartViewController: UIViewController {
+    
+    // demo flag
+    var demoFlag: DemoFlag = .newPost
     
     @IBOutlet var textView: UITextView! {
         didSet {
@@ -25,9 +29,27 @@ class StartViewController: UIViewController {
     
     
     @IBAction func didTouchNextButton() {
-        SVProgressHUD.show()
-        SVProgressHUD.dismiss(withDelay: 2.0) {
-           self.performSegue(withIdentifier: "toResult", sender: self)
+        switch demoFlag {
+        case .normalPost:
+            SVProgressHUD.show()
+            SVProgressHUD.dismiss(withDelay: 2.0) {
+                self.performSegue(withIdentifier: "toResult", sender: self)
+            }
+        case .badPost:
+            SVProgressHUD.show()
+            SVProgressHUD.dismiss(withDelay: 2.0) {
+                let alert = Alert(title: "Caution", text: "\n The post is not a good one.")
+                alert.showAlert()
+            }
+        case .newPost:
+            SVProgressHUD.show()
+            SVProgressHUD.dismiss(withDelay: 2.0) {
+                let alert = Alert(title: "Caution", text: "\n The post may be a new topic. \n Would you like to upload to Reddit Asthma?")
+                alert.showChoice(choice: "Upload", action: {
+                    SVProgressHUD.showSuccess(withStatus: "Successfully upload your post!\n Thank you.")
+                    SVProgressHUD.dismiss(withDelay: 2.0)
+                })
+            }
         }
     }
 }
